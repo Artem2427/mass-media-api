8dbecce915f29b56cba449d58d45f9a869917833
Artem Danko <vpeta4141@gmail.com>
2022-09-17 00:57:18 +0300

    feat: add user entity with base fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { typeOrmAsyncConfig } from './config/typeorm.config';
+
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/core/enums/userRole.enum.ts
@@ -0,0 +1,6 @@
+export enum UserRolesEnum {
+  Admin = 'admin',
+  Ghost = 'ghost',
+  Manager = 'manager',
+  Editor = 'editor',
+}
